7436d952c3c508aef8215a131f1b50697dc55408
Zakrok09 <eccyboo@protonmail.com>
2025-01-13 21:11:21 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Nix development flake for a project with dependencies.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+	    bun
+          ];
+
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ];
+
+          shellHook = ''
+            export WEBKIT_DISABLE_COMPOSITING_MODE=1
+          '';
+        };
+      });
+}
