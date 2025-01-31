a12d3d8e3cc47c10418155c039df71d0c9468343
Zakrok09 <31936449+Zakrok09@users.noreply.github.com>
2025-01-31 23:09:18 +0100

    chore: praxidike's prettier config

--- src/lib/components/SideBar.stories.svelte
+++ src/lib/components/SideBar.stories.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import {defineMeta} from "@storybook/addon-svelte-csf";
+    import { defineMeta } from "@storybook/addon-svelte-csf";
     import SideBar from "./SideBar.svelte";
 
-    const {Story} = defineMeta({
+    const { Story } = defineMeta({
         component: SideBar
-    })
+    });
 </script>
 
 <Story name="Base" args={{ projects: [] }} />
